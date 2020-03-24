7d3a11fc38f12dd6b772a8a0ab246b692278f44b
Olivier Jacques <olivier.jacques@dxc.com>
2020-03-24 15:04:26 +0100

    Remove messages when switching steps

--- online-devops-dojo/version-control/assets/step3.sh
+++
@@ -1,2 +0,0 @@
-source /tmp/shortname.txt
-echo -e "Go to the repository to see user stories: https://github.com/${SHORTNAME}/pet-clinic/issues"
--- online-devops-dojo/version-control/assets/step5.sh
+++
@@ -1,2 +0,0 @@
-source /tmp/shortname.txt
-echo -e "Go to the repository to create a pull request: https://github.com/${SHORTNAME}/pet-clinic"
