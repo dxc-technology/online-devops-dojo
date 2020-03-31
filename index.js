7ca27f39bfaecfd0eef78b150da56da37dfeb7a3
Philippe Berthonneau <philippe.berthonneau@dxc.com>
2020-03-31 17:26:32 +0200

    log bot name

--- index.js
+++ index.js
@@ -3,19 +3,21 @@ module.exports = app => {
   app.on('issue_comment.created', async context => {
     const { github, payload } = context
     const isPR = !!payload.issue.pull_request
+    const isIssue = !!payload.issue.pull_request
+
     const comment = payload.comment
     const user = comment.user
     const commentBody = comment.body && comment.body.toLowerCase()
     const userName = user.login
-    const isBot = user.type === 'Bot'
+    const isBot = user.type === 'Bot'  // for a genuine user this 'User'
     const {owner, repo, number} = context.issue()
   
-    // Let's not make anything if it is our Bot
+    // Let's not make anything if comment was made by a bot
     if (isBot) {
       return
     }
 
-    // We only want this on pet-clinic repository
+    // We only care about comments on pet-clinic repository
     if (repo != 'pet-clinic') {
       app.log('Skipping - not pet-clinic repository.')
       return
@@ -38,9 +40,11 @@ module.exports = app => {
     })
     app.log('Found ' + botComments + ' comments by the bot in this PR ' + comment.user.type)
 
+    // Note that GitHub usernames paulo and tina seem reserved for years by people who have never used their account for 3 and 10 years.
+
     // Version control module
     if (commentBody.match(/@paulo\s.*(?:review|check|verify)/)){
-      switch (botComments) {
+      switch (countBotComments('paulo')) {
         case 0:
           issuesComment = context.issue({ body: '![Paulo](https://s3.amazonaws.com/devopsdojoassets/paulo.png)\n Sure! I looked at the changes, and Brenda wants us to not only have horses but also ponies. So, we need to add `pony` in addition to `horse`. Can you do that?' })
           ret = context.github.issues.createComment(issuesComment) 
@@ -55,9 +59,7 @@ module.exports = app => {
     } 
     // Continuous integration module
     else if (commentBody.match(/@tina\s.*(?:review|check|verify|look|done|finish)/)){
-      app.log('Tina')
-
-      switch (botComments) {
+      switch (countBotComments('tina')) {
         case 0:
           issuesComment = context.issue({ body: '![Tina](https://s3.amazonaws.com/devopsdojoassets/tina.png)\n That looks good. Yet, I think we should replace `Jolly Jumper` by `Silver Blaze`. Can you make the change?' })
           ret = context.github.issues.createComment(issuesComment)
@@ -68,6 +70,20 @@ module.exports = app => {
           break;
       }
       return ret // Return the last result. Not very accurate, but will do.
+    }
+  })
+}
+
+// Get the list of comments for the bot named botName
+function countBotComments(botName) {
+  // Note that warning about deprecated number instead of issue_number could only be fixed with next major Probot release (10)
+  // https://github.com/probot/probot/pull/926
+  const issueComments = await context.github.issues.listComments({owner, repo, number})
+  botComments = 0
+  issueComments.data.forEach(function(comment) {
+    if (comment.user.type == 'Bot') {
+      botComments++
+      app.log('botName:'+ comment.user.login)
     }
   })
 }
