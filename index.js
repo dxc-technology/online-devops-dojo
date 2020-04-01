49c303e8b12190759c9e3ad5c99fd992c51366a5
Philippe Berthonneau <philippe.berthonneau@dxc.com>
2020-04-01 12:28:55 +0200

    js lint

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const coachName = "online-devops-dojo-coach"
+const coachName = 'online-devops-dojo-coach'
 
 module.exports = app => {
   app.log('Yay! The DevOps Dojo coach was run!')
@@ -9,8 +9,8 @@ module.exports = app => {
     const comment = payload.comment
     const user = comment.user
     const commentBody = comment.body && comment.body.toLowerCase()
-    const isBot = user.type === 'Bot'  // for a genuine user this would be 'User'
-    const {owner, repo, number} = context.issue()
+    const isBot = user.type === 'Bot' // for a genuine user this would be 'User'
+    const { owner, repo, number } = context.issue()
 
     // Let's not do anything if comment was made by a bot
     if (isBot) {
@@ -35,46 +35,40 @@ module.exports = app => {
     // Get the list of comments in this PR
     // Note that warning about deprecated number instead of issue_number could only be fixed with next major Probot release (10)
     // https://github.com/probot/probot/pull/926
-    const issueComments = await context.github.issues.listComments({owner, repo, number})
-    botComments = 0
-    issueComments.data.forEach(function(comment) {
+    var issueComments = await context.github.issues.listComments({ owner, repo, number })
+    var botComments = 0
+    issueComments.data.forEach(function (comment) {
       if (comment.user.type == 'Bot' && comment.user.login.startsWith(coachName)) {
         botComments++
       }
     })
-    app.log('Found ' + botComments + ' comment' + ((botComments>1) ? 's':'') +' by bot ' + coachName + ' in this PR.')
+    app.log('Found ' + botComments + ' comment' + ((botComments > 1) ? 's' : '') + ' by bot ' + coachName + ' in this PR.')
 
     // Note that GitHub usernames paulo and tina were reserved by people who have never used their account for 3 and 10 years.
 
+    var issuesComment
     // Version control module
-    if (commentBody.match(/(?:@|\/)paulo\s.*(?:review|check|verify)/)){
-
+    if (commentBody.match(/(?:@|\/)paulo\s.*(?:review|check|verify)/)) {
       switch (botComments) {
         case 0:
           issuesComment = context.issue({ body: '![Paulo](https://s3.amazonaws.com/devopsdojoassets/paulo.png)\n Sure! I looked at the changes, and Brenda wants us to not only have horses but also ponies. So, we need to add `pony` in addition to `horse`. Can you do that?' })
-          ret = context.github.issues.createComment(issuesComment) 
-          break;
+          return context.github.issues.createComment(issuesComment)
         case 1:
           issuesComment = context.issue({ body: '![Paulo](https://s3.amazonaws.com/devopsdojoassets/paulo.png)\n Looks good :+1: ! Merging this pull request.' })
-          ret = context.github.issues.createComment(issuesComment) 
-          ret = await context.github.pullRequests.merge({ owner, repo, number });
-          break;
+          context.github.issues.createComment(issuesComment)
+          return await context.github.pullRequests.merge({ owner, repo, number })
       }
-      return ret // Return the last result. Not very accurate, but will do.
     }
     // Continuous integration module
-    else if (commentBody.match(/(?:@|\/)tina\s.*(?:review|check|verify|look|done|finish)/)){
+    else if (commentBody.match(/(?:@|\/)tina\s.*(?:review|check|verify|look|done|finish)/)) {
       switch (botComments) {
         case 0:
           issuesComment = context.issue({ body: '![Tina](https://s3.amazonaws.com/devopsdojoassets/tina.png)\n That looks good. Yet, I think we should replace `Jolly Jumper` by `Silver Blaze`. Can you make the change?' })
-          ret = context.github.issues.createComment(issuesComment)
-          break;
+          return context.github.issues.createComment(issuesComment)
         case 1:
           issuesComment = context.issue({ body: '![Tina](https://s3.amazonaws.com/devopsdojoassets/tina.png)\n :+1: thanks!' })
-          ret = context.github.issues.createComment(issuesComment)
-          break;
+          return context.github.issues.createComment(issuesComment)
       }
-      return ret // Return the last result. Not very accurate, but will do.
     }
   })
 }
