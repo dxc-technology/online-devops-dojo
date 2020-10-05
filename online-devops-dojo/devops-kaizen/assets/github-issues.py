5bd7c0d4d4adaca6a7b99229f1b1ed1b197795fb
Tom Halpin <tom.halpin@dxc.com>
2020-10-05 22:33:36 +0100

    added value stream mapping and devops kaizen modules

--- online-devops-dojo/devops-kaizen/assets/github-issues.py
+++ online-devops-dojo/devops-kaizen/assets/github-issues.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 # Python helper to create GitHub issues, except if they already exist
 
-import os
-import sys
-import json
-import requests
-import yaml
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import os, sys, json, requests, yaml
 
 # Color constants
 # Reference: https://gist.github.com/chrisopedia/8754917
-COLINFO = "\033[0;35m"
-COLRESET = "\033[m"
+COLINFO="\033[0;35m"
+COLRESET="\033[m"
 
-baseurl = 'https://github.com/api/v3'
-headers = {"Content-Type": "application/json"}
+baseurl = 'https://api.github.com'
+headers = {"Content-Type": "application/json", "Accept": "application/vnd.github.symmetra-preview+json", "Accept": "application/vnd.github.v3+json"}
 user = os.environ['SHORTNAME']
 token = os.environ['TOKEN']
 repo = user + '/pet-clinic'
@@ -21,7 +21,6 @@ repo = user + '/pet-clinic'
 if len(sys.argv) != 2:
     print("   Usage: " + sys.argv[0] + " github-issues.yaml")
     sys.exit(0)
-
 
 def main():
     print(COLINFO + "Create user stories as GitHub issues..." + COLRESET)
@@ -34,52 +33,46 @@ def main():
         issues = yaml.load_all(open(issues_file, 'r'), Loader=yaml.FullLoader)
     except yaml.YAMLError as exc:
         print(COLINFO + exc + COLRESET)
-
-    # ID of the issue which was just created
-    prev_issue = 0
-    # List all open issues
-    payload = json.dumps(
-        {
-            "state": "open"
-        })
-    sys.stdout.write(COLINFO + "." + COLRESET)
-    sys.stdout.flush()
-    response = requests.get(baseurl + "/repos/" + repo + "/issues",
-                            data=payload,
-                            headers=headers,
-                            auth=(user, token))
-    issues_opened = []
-    if response.status_code == 200:
-        issues_opened = response.json()
+    
     # Create the issues
     for issue in issues:
-        # Avoid creating an issue which is already there (same title)
+        # Avoid creating an issue which is already there (same title) 
         issue_already_exists = False
-        for existing_issue in issues_opened:
-            if (existing_issue['title'] == issue['title']):
-                # This issue has the same title has the one we want to create. Skipping.
-                issue_already_exists = True
+        # List all open issues
+        payload = json.dumps({
+            "state" : "open"
+            })
+        sys.stdout.write(COLINFO + "." + COLRESET)
+        sys.stdout.flush()
+        response = requests.get(baseurl + "/repos/" + repo + "/issues", 
+            data=payload, 
+            headers=headers, 
+            auth=(user, token))
+        if response.status_code == 200:
+            issues_opened = json.loads(response.text)
+            for issue_found in issues_opened:
+                if (issue_found['title'] == issue['title']):
+                    # This issue has the same title has the one we want to create. Skipping.
+                    issue_already_exists = True
 
         if (not issue_already_exists):
-            payload = json.dumps(
-                {
-                    "title": issue['title'],
-                    "body": issue['body'].replace("[previous_issue]", str(prev_issue)),
-                    "labels": issue['labels']
+            payload = json.dumps({
+                "title" : issue['title'],
+                "body": issue['body'],
+                "labels" : issue['labels']
                 })
-            response = requests.post(baseurl + "/repos/" + repo + "/issues",
-                                     data=payload,
-                                     headers=headers,
-                                     auth=(user, token))
+            response = requests.post(baseurl + "/repos/" + repo + "/issues", 
+                data=payload, 
+                headers=headers, 
+                auth=(user, token))
             if response.status_code != 201:
-                # An error occurred
+                # An error occured
                 print(COLINFO + "Error adding issue " + issue['title'] + ": " + str(response.status_code) + " " + response.text + COLRESET)
             else:
-                issue_data = response.json()
-                prev_issue = issue_data['number']
                 sys.stdout.write(COLINFO + "." + COLRESET)
                 sys.stdout.flush()
     print(COLRESET)
 
 
 main()
+
